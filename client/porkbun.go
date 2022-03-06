9f031373b02a86bf6c4f28adc84ed866cc9650ef
JP Ungaretti <hello@jungaretti.com>
2022-03-06 09:22:37 +0000

    Condense client code

--- client/porkbun.go
+++ client/porkbun.go
@@ -1,5 +1,20 @@
 package client
 
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+var PK_PING = "https://porkbun.com/api/json/v3/ping"
+var PK_DNS_RETRIEVE = "https://porkbun.com/api/json/v3/dns/retrieve/"
+
+type Auth struct {
+	ApiKey       string `json:"apikey"`
+	SecretApiKey string `json:"secretapikey"`
+}
+
 func Ping(auth Auth) (string, error) {
 	body, err := postAndDecode(auth, PK_PING)
 	if err != nil {
@@ -16,4 +31,18 @@ func RetrieveRecords(auth Auth, domain string) (string, error) {
 	}
 
 	return string(body), nil
+}
+
+func postAndDecode(auth Auth, url string) ([]byte, error) {
+	json, err := json.Marshal(auth)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(json))
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadAll(resp.Body)
 }
