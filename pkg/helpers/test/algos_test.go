941111640a93587f3e4a10d36501defe43b604e6
JP Ungaretti <hello@jungaretti.com>
2022-05-02 00:15:50 -0700

    Add empty difference

--- pkg/helpers/test/algos_test.go
+++ pkg/helpers/test/algos_test.go
@@ -19,3 +19,18 @@ func TestDifference(t *testing.T) {
 		}
 	}
 }
+
+func TestDifferenceEmpty(t *testing.T) {
+	a := []int{1, 2, 3}
+	b := []int{7, 8, 9}
+
+	expected := []int{}
+	actual := helpers.Difference(a, b)
+
+	for index, value := range expected {
+		if value != actual[index] {
+			t.Log("expected", value, "found", actual[index])
+			t.Fail()
+		}
+	}
+}
