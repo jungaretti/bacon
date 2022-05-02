9fec53b1bbb72d0d579e30eacc37ce2bd97903ec
JP Ungaretti <hello@jungaretti.com>
2022-05-02 01:29:41 -0700

    Add difference with hasher

--- pkg/helpers/test/algos_test.go
+++ pkg/helpers/test/algos_test.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"bacon/pkg/helpers"
+	"fmt"
 	"testing"
 )
 
@@ -26,6 +27,36 @@ func TestDifferenceEmpty(t *testing.T) {
 
 	expected := []int{}
 	actual := helpers.Difference(a, b)
+
+	for index, value := range expected {
+		if value != actual[index] {
+			t.Log("expected", value, "found", actual[index])
+			t.Fail()
+		}
+	}
+}
+
+func TestDifferenceByHasher(t *testing.T) {
+	a := []int{1, 2, 3, 4, 5, 6, 7}
+	b := []int{6, 7, 8, 9}
+
+	expected := []int{1, 2, 3, 4, 5}
+	actual := helpers.DifferenceByHasher(a, b, func(t int) string { return fmt.Sprint(t) })
+
+	for index, value := range expected {
+		if value != actual[index] {
+			t.Log("expected", value, "found", actual[index])
+			t.Fail()
+		}
+	}
+}
+
+func TestDifferenceByHasherEmpty(t *testing.T) {
+	a := []int{1, 2, 3}
+	b := []int{7, 8, 9}
+
+	expected := []int{}
+	actual := helpers.DifferenceByHasher(a, b, func(t int) string { return fmt.Sprint(t) })
 
 	for index, value := range expected {
 		if value != actual[index] {
