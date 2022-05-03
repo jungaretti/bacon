c6a95e1a34988c13611f8f87512a3576dad61d99
JP Ungaretti <hello@jungaretti.com>
2022-05-03 02:54:27 +0000

    Remove fuzzy compare

--- pkg/porkbun/record.go
+++ pkg/porkbun/record.go
@@ -16,36 +16,6 @@ type PorkbunRecord struct {
 	Notes    string `json:"notes"`
 }
 
-func (left *PorkbunRecord) FuzzyCompare(right *PorkbunRecord) bool {
-	// Ignore IDs and notes
-	return (left.Name == right.Name &&
-		left.Type == right.Type &&
-		left.Content == right.Content &&
-		left.TTL == right.TTL &&
-		left.Priority == right.Priority)
-}
-
-func (left *PorkbunRecord) FuzzyCompareToClientRecord(right *client.Record) bool {
-	// Left must be a valid int and equal to right
-	if left.TTL != "" && right.TTL != 0 {
-		ttl, err := strconv.Atoi(left.TTL)
-		if err != nil || int(ttl) != right.TTL {
-			return false
-		}
-	}
-	if left.Priority != "" && right.Priority != 0 {
-		priority, err := strconv.Atoi(left.Priority)
-		if err != nil || int(priority) != right.Priority {
-			return false
-		}
-	}
-
-	// Ignore IDs and notes
-	return (left.Name == right.Host &&
-		left.Type == right.Type &&
-		left.Content == right.Content)
-}
-
 func (src *PorkbunRecord) FuzzyHash() string {
 	return fmt.Sprint(src.Name, src.Type, src.Content, src.TTL, src.Priority)
 }
