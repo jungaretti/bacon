2afc2735fa070b29ce629d9f2b9d49b27150f7b6
JP Ungaretti <git@jungaretti.com>
2023-08-20 13:28:58 -0700

    Make bacon before using it

--- test/test.bash
+++ test/test.bash
@@ -7,6 +7,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LIB_DIR="$SCRIPT_DIR/lib"
 LIB_FILE="$LIB_DIR/lib.bash"
 
+make
+
 ./bin/bacon ping
 ./bin/bacon deploy config.example.yml
 ./bin/bacon deploy --delete --create config.example.yml
