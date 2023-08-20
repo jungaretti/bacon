7359391222d59518d6423c0b63f002f3f90b6dac
JP Ungaretti <git@jungaretti.com>
2023-08-20 13:27:54 -0700

    Add simple test setup

---
+++ test/test.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Find directory of script file
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LIB_DIR="$SCRIPT_DIR/lib"
+LIB_FILE="$LIB_DIR/lib.bash"
+
+./bin/bacon ping
+./bin/bacon deploy config.example.yml
+./bin/bacon deploy --delete --create config.example.yml
+
+source $LIB_FILE
