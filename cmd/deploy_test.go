2eaa286b33b4871e269836cbf23a47ae7403d883
JP Ungaretti <jp@jungaretti.com>
2024-09-04 05:21:18 +0000

    Rename records variable

--- cmd/deploy_test.go
+++ cmd/deploy_test.go
@@ -33,13 +33,13 @@ records:
 		t.Fatal("did not deploy records", err)
 	}
 
-	recordsAfterDeployment, err := provider.AllRecords("plantbasedbacon.xyz")
+	records, err := provider.AllRecords("plantbasedbacon.xyz")
 	if err != nil {
 		t.Fatal("could not fetch records after deployment", err)
 	}
 
-	if len(recordsAfterDeployment) != 2 {
-		t.Fatal("expected 2 records after deployment, got", len(recordsAfterDeployment))
+	if len(records) != 2 {
+		t.Fatal("expected 2 records after deployment, got", len(records))
 	}
 }
 
