0c795c8fdf0ce19eed15c529db776bdcd00ced73
JP Ungaretti <jp@jungaretti.com>
2024-12-13 16:48:57 -0800

    Construct app in root.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,17 @@
 package cmd
 
 import (
+	"bacon/pkg/providers/porkbun"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/subosito/gotenv"
 )
 
-func Execute(app *App) {
+func Execute() {
+	app := newPorkbunApp()
+
 	root := newRootCmd(app)
 
 	if err := root.Execute(); err != nil {
@@ -28,4 +32,17 @@ func newRootCmd(app *App) *cobra.Command {
 	root.AddCommand(newPrintCmd(app))
 
 	return root
+}
+
+func newPorkbunApp() *App {
+	// Loads .env in the current directory
+	gotenv.Load()
+
+	var porkbunApiKey = os.Getenv("PORKBUN_API_KEY")
+	var porkbunSecretApiKey = os.Getenv("PORKBUN_SECRET_KEY")
+
+	app := App{
+		Provider: porkbun.NewPorkbunProvider(porkbunApiKey, porkbunSecretApiKey),
+	}
+	return &app
 }
--- main.go
+++ main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"bacon/cmd"
-	"bacon/pkg/providers/porkbun"
-	"os"
-
-	"github.com/subosito/gotenv"
 )
 
 func main() {
-	// Loads .env in the current directory
-	gotenv.Load()
-
-	var porkbunApiKey = os.Getenv("PORKBUN_API_KEY")
-	var porkbunSecretApiKey = os.Getenv("PORKBUN_SECRET_KEY")
-
-	app := cmd.App{
-		Provider: porkbun.NewPorkbunProvider(porkbunApiKey, porkbunSecretApiKey),
-	}
-
-	cmd.Execute(&app)
+	cmd.Execute()
 }
