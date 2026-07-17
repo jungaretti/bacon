1abeef9a57272a78e2db4be23579b11a2a173030
JP Ungaretti <jp@jungaretti.com>
2026-07-17 16:56:39 -0700

    Clean up config package

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -2,11 +2,12 @@ package config
 
 import (
 	"bacon/pkg/porkbun"
+	"os"
 	"testing"
 )
 
 func TestValidConfig(t *testing.T) {
-	configFile, err := SeedConfigToTempFile(`
+	configFile, err := seedConfigToTempFile(`
 domain: bacontest42.com
 records:
     - host: bacontest42.com
@@ -33,12 +34,12 @@ records:
 	}
 
 	if len(config.Records) != 3 {
-		t.Fatal("expected 3 records after deployment, got", len(config.Records))
+		t.Fatal("expected 3 records, got", len(config.Records))
 	}
 }
 
-func TestInvalidConfigMissingHost(t *testing.T) {
-	configFile, err := SeedConfigToTempFile(`
+func TestInvalidConfig(t *testing.T) {
+	configFile, err := seedConfigToTempFile(`
 domain: bacontest42.com
 records:
     - type: ALIAS
@@ -51,143 +52,7 @@ records:
 
 	_, err = ReadFile(configFile)
 	if err == nil {
-		t.Fatal("expected error when a record is missing host field", err)
-	}
-}
-
-func TestInvalidConfigMissingType(t *testing.T) {
-	configFile, err := SeedConfigToTempFile(`
-domain: bacontest42.com
-records:
-    - host: bacontest42.com
-      ttl: 600
-      content: pixie.porkbun.com
-`)
-	if err != nil {
-		t.Fatal("could not seed config to temp file", err)
-	}
-
-	_, err = ReadFile(configFile)
-	if err == nil {
-		t.Fatal("expected error when a record is missing type field", err)
-	}
-}
-
-func TestInvalidConfigInvalidType(t *testing.T) {
-	configFile, err := SeedConfigToTempFile(`
-domain: bacontest42.com
-records:
-    - host: bacontest42.com
-      type: FAKE
-      ttl: 600
-      content: pixie.porkbun.com
-`)
-	if err != nil {
-		t.Fatal("could not seed config to temp file", err)
-	}
-
-	_, err = ReadFile(configFile)
-	if err == nil {
-		t.Fatal("expected error when a record has an invalid type", err)
-	}
-}
-
-func TestInvalidConfigMissingTtl(t *testing.T) {
-	configFile, err := SeedConfigToTempFile(`
-domain: bacontest42.com
-records:
-    - host: bacontest42.com
-      type: ALIAS
-      content: pixie.porkbun.com
-`)
-	if err != nil {
-		t.Fatal("could not seed config to temp file", err)
-	}
-
-	_, err = ReadFile(configFile)
-	if err == nil {
-		t.Fatal("expected error when a record is missing ttl field", err)
-	}
-}
-
-func TestInvalidConfigInvalidTtl(t *testing.T) {
-	configFile, err := SeedConfigToTempFile(`
-domain: bacontest42.com
-records:
-    - host: bacontest42.com
-      type: ALIAS
-      ttl: 300
-      content: pixie.porkbun.com
-`)
-	if err != nil {
-		t.Fatal("could not seed config to temp file", err)
-	}
-
-	_, err = ReadFile(configFile)
-	if err == nil {
-		t.Fatal("expected error when a record has an invalid ttl", err)
-	}
-}
-
-func TestInvalidConfigMissingContent(t *testing.T) {
-	configFile, err := SeedConfigToTempFile(`
-domain: bacontest42.com
-records:
-    - host: bacontest42.com
-      type: ALIAS
-      ttl: 600
-`)
-	if err != nil {
-		t.Fatal("could not seed config to temp file", err)
-	}
-
-	_, err = ReadFile(configFile)
-	if err == nil {
-		t.Fatal("expected error when a record is missing content field", err)
-	}
-}
-
-func TestInvalidConfigInvalidPriority(t *testing.T) {
-	configFile, err := SeedConfigToTempFile(`
-domain: bacontest42.com
-records:
-    - host: bacontest42.com
-      type: ALIAS
-      ttl: 600
-      content: pixie.porkbun.com
-	  priority: 20
-`)
-	if err != nil {
-		t.Fatal("could not seed config to temp file", err)
-	}
-
-	_, err = ReadFile(configFile)
-	if err == nil {
-		t.Fatal("priority is not allowed on ALIAS records", err)
-	}
-}
-
-func TestInvalidConfigCnameSameHost(t *testing.T) {
-	configFile, err := SeedConfigToTempFile(`
-domain: bacontest42.com
-records:
-    - host: '*.bacontest42.com'
-      type: CNAME
-      ttl: 600
-      content: pixie.porkbun.com
-    - type: MX
-      host: "*.bacontest42.com"
-      content: in1-smtp.messagingengine.com
-      ttl: 600
-      priority: 10
-`)
-	if err != nil {
-		t.Fatal("could not seed config to temp file", err)
-	}
-
-	_, err = ReadFile(configFile)
-	if err == nil {
-		t.Fatal("cannot have a CNAME and another record for the same host", err)
+		t.Fatal("expected error when config is invalid", err)
 	}
 }
 
@@ -225,4 +90,20 @@ func TestRecordFromPorkbunWithoutPriority(t *testing.T) {
 	if configRecord.Priority != 0 {
 		t.Error("expected priority 0, got", configRecord.Priority)
 	}
+}
+
+func seedConfigToTempFile(mockConfig string) (string, error) {
+	tempFile, err := os.CreateTemp("", "tmpfile-*")
+	if err != nil {
+		return "", err
+	}
+
+	defer tempFile.Close()
+
+	_, err = tempFile.WriteString(mockConfig)
+	if err != nil {
+		return "", err
+	}
+
+	return tempFile.Name(), nil
 }
